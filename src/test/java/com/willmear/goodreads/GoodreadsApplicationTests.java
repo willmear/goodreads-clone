886cf33d0f5f4faa3a22b40c5d82224f0a4d8747
Will Mear <wxm158@student.bham.ac.uk>
2024-12-02 16:11:51 +0000

    initial commit

---
+++ src/test/java/com/willmear/goodreads/GoodreadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willmear.goodreads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodreadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
