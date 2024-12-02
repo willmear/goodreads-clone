7db69e0f29f88b8eeaefb4cad18e1243d6c4e364
Will Mear <wxm158@student.bham.ac.uk>
2024-12-02 16:43:00 +0000

    books data

---
+++ src/main/java/com/willmear/goodreads/domain/model/Oauth2UserInfoDto.java
@@ -0,0 +1,20 @@
+package com.willmear.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Oauth2UserInfoDto {
+
+    private String name;
+    private String id;
+    private String email;
+    private String picture;
+
+
+}
